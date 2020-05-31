6e5b2996ca963a8c85526be2a007b880f4810d50
Lourenco Flavio <flavlo@hotmail.fr>
2020-05-31 21:48:29 +0200

    2èmeDepots

---
+++ evenement.js
@@ -0,0 +1,16 @@
+
+class evenement {
+
+    constructor(desc, x, y, bonus){
+        this.description = desc;
+        this.x = x;
+        this.y = y;
+        this.nbreHeure = bonus;
+    }
+
+    setLog(texte){
+        this.descLog = texte;
+    }
+
+}
+// je n'ai pas réussi à intégrer les classes
